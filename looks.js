9aabb8dcf775c9a82f01e4c081e3ce60ecec268d
abanero1998-afk <abanero1998@icloud.com>
2026-09-20 19:31:28 +0200

    Collega le foto lookbook e i capi

--- looks.js
+++ looks.js
@@ -1,2 +1,11 @@
-window.LOOKS={casual:'',classico:''};
-// placeholder replaced next
+window.LOOKS = {
+  casual: "img/look-casual.jpg",
+  classico: "img/look-classico.jpg"
+};
+window.ITEM_PHOTOS = {
+  "nb1000": "img/nb1000.jpg",
+  "nb-cargo": "img/nb-cargo.jpg",
+  "nb-ls": "img/nb-ls.jpg",
+  "cam-fiori": "img/cam-fiori.jpg",
+  "pant-blu": "img/pant-blu.jpg"
+};
