cc73163b711b9c8ffc0e7c587828c60ee9eb8352
abanero1998-afk <abanero1998@icloud.com>
2026-09-20 19:54:09 +0200

    Foto prodotto visibili (CDN)

---
+++ item-photos.js
@@ -0,0 +1,7 @@
+window.ITEM_PHOTOS={
+  "nb1000":"https://images.unsplash.com/photo-1539185441755-769473a23570?auto=format&fit=crop&w=800&q=80",
+  "nb-cargo":"https://nb.scene7.com/is/image/NB/mp41579sot_nb_70_i?$pdpflexf2$",
+  "nb-ls":"https://nb.scene7.com/is/image/NB/mt51931pef_nb_70_i?$pdpflexf2$",
+  "cam-fiori":"https://images.unsplash.com/photo-1596755094514-f87e34085b2c?auto=format&fit=crop&w=800&q=80",
+  "pant-blu":"https://images.unsplash.com/photo-1594938291221-94f18cbb566e?auto=format&fit=crop&w=800&q=80"
+};
